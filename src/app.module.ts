2a9e16879449388f9db2ea8c088ff2ca985b1329
AhhYeaah <vivi26743@hotmail.com>
2023-10-09 06:51:17 -0300

    first_commit

---
+++ src/app.module.ts
@@ -0,0 +1,25 @@
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+
+import { AppLoggerMiddleware } from './middlewares/app-logger.middleware';
+import { UsersModule } from './users/users.module';
+import { APP_GUARD } from '@nestjs/core';
+import { AuthGuard } from './guards/auth/auth.guard';
+import { PrismaService } from './services/prisma.service';
+import { AuthModule } from './guards/auth/auth.module';
+
+@Module({
+  imports: [UsersModule, AuthModule],
+  controllers: [],
+  providers: [
+    PrismaService,
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+  ],
+})
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer): void {
+    consumer.apply(AppLoggerMiddleware).forRoutes('*');
+  }
+}
